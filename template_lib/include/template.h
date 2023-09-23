d5715d8527fdfcb2d447592d17cc34e3cacefc7b
Ilia <ostilia@mail.ru>
2023-09-23 16:44:03 +0300

    template init

---
+++ template_lib/include/template.h
@@ -0,0 +1,6 @@
+
+class Example {
+
+public:
+    void foo();
+};
---
+++ test_template/test.cpp
@@ -0,0 +1,16 @@
+#include <fstream>
+#include <iostream>
+#include <random>
+#include <sstream>
+
+#include <gtest/gtest.h>
+
+#include "template.h"
+
+TEST(TemplateLib, CreateClass) {
+    Example e;
+    e.foo();
+}
+
+
+
